3a8664b1d859130630fea620a4ca2c4748dd2006
eduardobio6 <eduardooliveira278@gmail.com>
2022-08-26 19:20:41 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-vemap5
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-vemap5)
---
+++ index.ts
@@ -0,0 +1,13 @@
+// Import stylesheets
+import './style.css';
+
+let List: number[] = [1, 2, 3];
+
+let otherList: Array<number> = [1, 2, 3];
+
+const appDiv: HTMLElement = document.getElementById('app.text');
+appDiv.innerHTML = `
+<h1>Array Declaration</h1>
+<h2> ${List}</h2>
+<h2> other list: ${otherList}</h2>
+`;
