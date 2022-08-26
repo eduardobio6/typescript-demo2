d22daf84de5755952b739472b19ea86b7b052e31
eduardobio6 <eduardooliveira278@gmail.com>
2022-08-26 19:30:00 -0300

    Exemplo de uma tupla em typescript

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 // Import stylesheets
 import './style.css';
 
-let List: number[] = [1, 2, 3];
+/*let List: number[] = [1, 2, 3];
 
 let otherList: Array<number> = [1, 2, 3];
 
@@ -11,4 +11,15 @@ appDiv.innerHTML = `
 <h1>Array Declaration</h1>
 <h2> ${List}</h2>
 <h2> other list: ${otherList}</h2>
+`;*/
+
+//Conceito de TUPLA
+let me: [string, number];
+
+me = ['Carlos Eduardo', 25];
+
+const appDiv: HTMLElement = document.getElementById('app.text');
+appDiv.innerHTML = `
+<h1> Tuple</h1>
+<h2>Me chamo ${me[0]} e tenho ${me[1]} anos</h2> 
 `;
