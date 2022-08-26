5d54e373c2eb4cee92538b10ec6125a61dc48af2
eduardobio6 <eduardooliveira278@gmail.com>
2022-08-26 19:22:36 -0300

    Exemplo de Strig Template

--- index.ts
+++ index.ts
@@ -6,6 +6,7 @@ let List: number[] = [1, 2, 3];
 let otherList: Array<number> = [1, 2, 3];
 
 const appDiv: HTMLElement = document.getElementById('app.text');
+//Exemplo de string template
 appDiv.innerHTML = `
 <h1>Array Declaration</h1>
 <h2> ${List}</h2>
